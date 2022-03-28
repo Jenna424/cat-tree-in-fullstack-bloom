0321ceebce830b49cd3e6a5402b4ba2ea2e9efc0
Jenna Leopold <jleopold424@gmail.com>
2022-03-28 11:57:53 -0400

    createCat working

--- controllers/index.js
+++ controllers/index.js
@@ -11,21 +11,32 @@ const getAllShelters = async (req, res) => {
 
 const createCat = async (req, res) => {
   try {
+    const cat = await new Cat(req.body)
+    await cat.save()
+    // Add the cat to the shelter's array of cats
+    // and update the catCount in the shelter
     const shelterId = req.body.shelter
     const shelter = await Shelter.findById(shelterId)
-    const cat = await new Cat(req.body)
-    // Add the cat to the shelter's array of cats
     await Shelter.findByIdAndUpdate(shelterId, {
-      cats: [...shelter.cats, cat._id]
+      cats: [...shelter.cats, cat._id],
+      catCount: shelter.catCount + 1
     })
-    // Update the catCount in the shelter
-    await Shelter.findByIdAndUpdate(shelterId, {
-      catCount: shelter.cats.length + 1
-    })
-    await cat.save()
     return res.status(201).json(cat)
   } catch (error) {
     return res.status(500).json({ error: error.message })
+  }
+}
+// Fetches all cats in a particular shelter
+const getCatsInShelter = async (req, res) => {
+  try {
+    const shelter = await Shelter.findById(req.params.id)
+    const shelterCats = []
+    for await (const catId of shelter.cats) {
+      shelterCats.push(await Cat.findById(catId))
+    }
+    return res.status(201).json({ shelterCats })
+  } catch (error) {
+    return res.status(500).send(error.message)
   }
 }
 
