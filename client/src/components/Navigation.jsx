fd198599fb5f9fe0fb41cee9b2065433e0820db9
Jenna Leopold <jleopold424@gmail.com>
2022-03-28 14:44:33 -0400

    created Navigation component

---
+++ client/src/components/Navigation.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Navigation = () => {
+  return (
+    <header>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="about">About</Link>
+      </nav>
+    </header>
+  )
+}
+
+export default Navigation
