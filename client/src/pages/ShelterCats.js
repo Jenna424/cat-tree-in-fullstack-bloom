18375b4f814423365f45abf194205d1e2aebcd27
Jenna Leopold <jleopold424@gmail.com>
2022-03-28 19:31:51 -0400

    get cats by shelter id in ShelterCats.js

---
+++ client/src/pages/ShelterCats.js
@@ -0,0 +1,26 @@
+import { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import axios from 'axios'
+
+const ShelterCats = () => {
+  let { id } = useParams()
+  const [cats, setCats] = useState([])
+  const getCatsByShelterId = async () => {
+    const response = await axios.get(
+      `http://localhost:3001/shelters/${id}/cats`
+    )
+    setCats(response.data)
+  }
+
+  useEffect(() => {
+    getCatsByShelterId()
+  }, [])
+
+  return (
+    <div>
+      <h3>Cats Found in Shelter</h3>
+    </div>
+  )
+}
+
+export default ShelterCats
