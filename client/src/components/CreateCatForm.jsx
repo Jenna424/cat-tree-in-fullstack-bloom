429b9c84d8d80064a865bfa9229a180f6ae316ba
Jenna Leopold <jleopold424@gmail.com>
2022-03-29 16:01:28 -0400

    add required attribute to inputs in Create Cat Form

--- client/src/components/CreateCatForm.jsx
+++ client/src/components/CreateCatForm.jsx
@@ -5,23 +5,23 @@ const CreateCatForm = ({ newCat, handleInputChange, createCat }) => {
       <form onSubmit={createCat}>
         <div className="form-field">
           <label><b>Name</b>:</label>
-          <input name="name" type="text" onChange={handleInputChange} placeholder="Enter name..." value={newCat.name} />
+          <input name="name" type="text" onChange={handleInputChange} placeholder="Enter name..." value={newCat.name} required />
         </div>
         <div className="form-field">
           <label><b>Breed</b>:</label>
-          <input name="breed" type="text" onChange={handleInputChange} placeholder="Enter breed..." value={newCat.breed} />
+          <input name="breed" type="text" onChange={handleInputChange} placeholder="Enter breed..." value={newCat.breed} required />
         </div>
         <div className="form-field">
           <label><b>Color</b>:</label>
-          <input name="color" type="text" onChange={handleInputChange} placeholder="Enter color..." value={newCat.color} />
+          <input name="color" type="text" onChange={handleInputChange} placeholder="Enter color..." value={newCat.color} required />
         </div>
         <div className="form-field">
           <label><b>Temperament</b>:</label>
-          <input name="temperament" type="text" onChange={handleInputChange} placeholder="Enter temperament..." value={newCat.temperament} />
+          <input name="temperament" type="text" onChange={handleInputChange} placeholder="Enter temperament..." value={newCat.temperament} required />
         </div>
         <div className="form-field">
           <label><b>Image</b>:</label>
-          <input name="image" type="text" onChange={handleInputChange} placeholder="Enter image path..." value={newCat.image} />
+          <input name="image" type="text" onChange={handleInputChange} placeholder="Enter image path..." value={newCat.image} required />
         </div>
         <div className="button-wrapper">
           <input type="submit" />
