3db4b500b4016fe2db4a0aac466783bbcc8888a5
Jenna Leopold <jleopold424@gmail.com>
2022-03-29 09:52:39 -0400

    add line breaks after each label in form

--- client/src/components/CreateCatForm.jsx
+++ client/src/components/CreateCatForm.jsx
@@ -5,22 +5,27 @@ const CreateCatForm = ({ newCat, handleInputChange, createCat }) => {
       <form onSubmit={createCat}>
         <div className="form-field">
           <label><b>Name</b>:</label>
+          <br />
           <input name="name" type="text" onChange={handleInputChange} placeholder="Enter name..." value={newCat.name} />
         </div>
         <div className="form-field">
           <label><b>Breed</b>:</label>
+          <br />
           <input name="breed" type="text" onChange={handleInputChange} placeholder="Enter breed..." value={newCat.breed} />
         </div>
         <div className="form-field">
           <label><b>Color</b>:</label>
+          <br />
           <input name="color" type="text" onChange={handleInputChange} placeholder="Enter color..." value={newCat.color} />
         </div>
         <div className="form-field">
           <label><b>Temperament</b>:</label>
+          <br />
           <input name="temperament" type="text" onChange={handleInputChange} placeholder="Enter temperament..." value={newCat.temperament} />
         </div>
         <div className="form-field">
           <label><b>Image</b>:</label>
+          <br />
           <input name="image" type="text" onChange={handleInputChange} placeholder="Enter image path..." value={newCat.image} />
         </div>
       </form>
