1b5ea9091e72ba954285aa5ef0fa8545e4dd5f16
Jenna Leopold <jleopold424@gmail.com>
2022-03-28 21:05:55 -0400

    added Home page

---
+++ client/src/pages/Home.js
@@ -0,0 +1,32 @@
+import { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import axios from 'axios'
+import ShelterCard from '../components/ShelterCard'
+
+const Home = () => {
+  const [shelters, setShelters] = useState([])
+
+  const fetchShelters = async () => {
+    const response = await axios.get('http://localhost:3001/shelters')
+    setShelters(response.data.shelters)
+  }
+
+  useEffect(() => {
+    fetchShelters()
+  }, [])
+
+  return (
+    <div>
+      <h1>Cat Sanctuaries</h1>
+      <div className="shelters-wrapper">
+        {shelters.map((shelter) => (
+          <Link to={`/shelters/${shelter._id}/cats`} key={shelter._id}>
+            <ShelterCard {...shelter} />
+          </Link>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default Home
