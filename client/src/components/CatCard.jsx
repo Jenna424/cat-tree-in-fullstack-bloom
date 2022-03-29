4405b9b7acd05e08c5b386049d6a55d4c21e4a46
Jenna Leopold <jleopold424@gmail.com>
2022-03-28 20:46:16 -0400

    destructure deleteCat prop & add 'Remove from Shelter' button

--- client/src/components/CatCard.jsx
+++ client/src/components/CatCard.jsx
@@ -1,4 +1,4 @@
-const CatCard = ({ _id, name, breed, color, temperament, image }) => {
+const CatCard = ({ _id, name, breed, color, temperament, image, deleteCat }) => {
   return (
     <div className="cat-card-container">
       <h3>{name}</h3>
@@ -6,7 +6,7 @@ const CatCard = ({ _id, name, breed, color, temperament, image }) => {
       <p className="cat-details"><b>Breed</b>: {breed}</p>
       <p className="cat-details"><b>Color</b>: {color}</p>
       <p className="cat-details"><b>Temperament</b>: {temperament}</p>
-      <button className="delete-cat-btn">Remove from Shelter</button>
+      <button className="delete-cat-btn" onClick={() => deleteCat(_id)}>Remove from Shelter</button>
     </div>
   )
 }
