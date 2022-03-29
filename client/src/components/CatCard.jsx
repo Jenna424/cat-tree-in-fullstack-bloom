2ba05c14815c2a6d7f270809fb145385a7be184d
Jenna Leopold <jleopold424@gmail.com>
2022-03-29 16:15:53 -0400

    destructure updateCat prop & add Remove Name button

--- client/src/components/CatCard.jsx
+++ client/src/components/CatCard.jsx
@@ -1,4 +1,4 @@
-const CatCard = ({ _id, name, breed, color, temperament, image, deleteCat }) => {
+const CatCard = ({ _id, name, breed, color, temperament, image, updateCat, deleteCat }) => {
   return (
     <div className="cat-card">
       <h3>{name}</h3>
@@ -6,6 +6,7 @@ const CatCard = ({ _id, name, breed, color, temperament, image, deleteCat }) => 
       <p className="cat-details"><b>Breed</b>: {breed}</p>
       <p className="cat-details"><b>Color</b>: {color}</p>
       <p className="cat-details"><b>Temperament</b>: {temperament}</p>
+      <button className="update-cat-btn" disabled={name === 'Anonymous'} onClick={() => updateCat(_id)}>Remove Name</button>
       <button className="delete-cat-btn" onClick={() => deleteCat(_id)}>Remove from Shelter</button>
     </div>
   )
