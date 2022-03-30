c0abc43ff35e2fd5b52740a7c8af1d34962c5175
Jenna Leopold <jleopold424@gmail.com>
2022-03-30 12:12:05 -0400

    added About page

---
+++ client/src/pages/About.js
@@ -0,0 +1,42 @@
+const About = () => {
+  return (
+    <div className="about-page-wrapper">
+      <h1 className="title">Cat Tree in Fullstack Bloom</h1>
+      <h4>
+        <em>
+          An app that facilitates the transfer of rescue cats from shelters to
+          their fur-ever homes
+        </em>
+      </h4>
+      <div className="about-page-body">
+        <div className="cat-tree-image-wrapper">
+          <img
+            className="cat-tree-img"
+            src="https://i.imgur.com/U3GcEQ4.jpeg"
+            alt="cat tree"
+          />
+        </div>
+        <div className="instructions">
+          <p className="instructions-paragraph">
+            Please follow the instructions below:
+          </p>
+          <p>Access information about each cat sanctuary on the homepage.</p>
+          <p>Click on an individual shelter to view its available cats.</p>
+          <p>
+            Admit a new cat to the shelter by submitting the form that appears
+            on button click.
+          </p>
+          <p>
+            Change a cat’s name to ‘Anonymous’, in case its new family wishes to
+            rename it.
+          </p>
+          <p>
+            Remove a cat from the shelter on button click once it is adopted.
+          </p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default About
