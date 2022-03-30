84771fbc2a372258e3fc3d6eeffb2ad66b878f7d
Jenna Leopold <jleopold424@gmail.com>
2022-03-30 10:09:18 -0400

    import About component & wire up about path route in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import { Routes, Route } from 'react-router-dom'
 import Navigation from './components/Navigation'
 import Home from './pages/Home'
-// import About from './pages/About'
+import About from './pages/About'
 import ShelterCats from './pages/ShelterCats'
 import './App.css'
 // import './Style.css'
@@ -13,7 +13,7 @@ const App = () => {
       <main>
         <Routes>
           <Route path="/" element={<Home />} />
-          {/* <Route path="about" element={<About />} /> */}
+          <Route path="about" element={<About />} />
           <Route path="shelters/:id/cats" element={<ShelterCats />} />
         </Routes>
       </main>
