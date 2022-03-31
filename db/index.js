f86ab4d2c017748ad426aa4e1f5527c013c85a90
Jenna Leopold <jleopold424@gmail.com>
2022-03-31 15:39:59 -0400

    delete commented out MONGODB_URI variable

--- db/index.js
+++ db/index.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose')
 require('dotenv').config()
 
-// let MONGODB_URI = 'mongodb://127.0.0.1:27017/catSanctuaryDatabase'
 let dbUrl =
   process.env.NODE_ENV === 'production'
     ? process.env.MONGODB_URI
