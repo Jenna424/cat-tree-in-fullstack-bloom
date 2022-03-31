ebea9727c228e66e0662f9c09ebf74f309830c63
Jenna Leopold <jleopold424@gmail.com>
2022-03-31 14:50:44 -0400

    deploy to Heroku

--- db/index.js
+++ db/index.js
@@ -1,9 +1,14 @@
 const mongoose = require('mongoose')
+require('dotenv').config()
 
-let MONGODB_URI = 'mongodb://127.0.0.1:27017/catSanctuaryDatabase'
+// let MONGODB_URI = 'mongodb://127.0.0.1:27017/catSanctuaryDatabase'
+let dbUrl =
+  process.env.NODE_ENV === 'production'
+    ? process.env.MONGODB_URI
+    : 'mongodb://127.0.0.1:27017/catSanctuaryDatabase'
 
 mongoose
-  .connect(MONGODB_URI, { useUnifiedTopology: true, useNewUrlParser: true })
+  .connect(dbUrl)
   .then(() => {
     console.log('Successfully connected to MongoDB.')
   })
